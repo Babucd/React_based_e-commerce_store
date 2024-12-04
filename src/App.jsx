96b8ee9a87aac728b3e826c6030b6f37e51f3ec3
Babu Natarajan <153248698+Babucd@users.noreply.github.com>
2024-12-04 17:00:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,31 +25,3 @@ const App = () => {
 };
 
 export default App;
-
-// create footer that contains four consist of five
-
-// 1. brand name
-// locaton : left
-// decripction
-
-// 2. shop
-// location after the left more bit space between these two
-// backpacks
-// luggage
-// accessories
-
-// 3. about
-// location after the shop
-// about us
-// quliry
-// shops
-
-// 4 . more
-// location after the about
-// career
-// press
-// pricacy policy
-
-// 5. support
-// contact usFAQ
-// teams & conditions
