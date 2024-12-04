e7853dbef9e65dbbd9eb3f601b85a97aab0c34c0
Babu Natarajan <153248698+Babucd@users.noreply.github.com>
2024-12-04 17:01:39 +0530

    Update Main.jsx
    just removed the comments

--- src/coponentes/Main.jsx
+++ src/coponentes/Main.jsx
@@ -2,25 +2,21 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./Mainimg.jpg";
 
-// Apply Poppins font globally within this component
 const Main = () => {
   return (
     <div
-      className="container-fluid" // Use container-fluid for full width
+      className="container-fluid"
       style={{
-        fontFamily: "'Poppins', sans-serif", // Set the Poppins font
-        backgroundColor: "#f0f0f0", // Light gray background
-        height: "100vh", // Full page height
-        display: "flex", // Use flexbox for layout
-        justifyContent: "center", // Center horizontally
-        alignItems: "center", // Center vertically
-        padding: "0", // Remove default padding
+        fontFamily: "'Poppins', sans-serif",
+        backgroundColor: "#f0f0f0",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "0",
       }}
     >
       <div className="row align-items-center" style={{ width: "80%" }}>
-        {" "}
-        {/* Adjust the width of the row */}
-        {/* Left side text section */}
         <div className="col-md-6 text-section">
           <h1 className="display-4 fw-bold text-dark">
             Adventure Awaits: Your Ultimate Backpack Destination
@@ -29,11 +25,8 @@ const Main = () => {
             Gear Up and Go: Explore Our Backpack Selection
           </p>
         </div>
-        {/* Right side image section */}
         <div className="col-md-6 position-relative image-section">
           <img src={logo} alt="Backpack" className="img-fluid rounded" />
-
-          {/* Product Tag Below the Image */}
         </div>
       </div>
     </div>
